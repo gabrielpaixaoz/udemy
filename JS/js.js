d8a74a6f89f24df9c97af6a5cfad3c862af58af0
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-03-22 09:50:39 -0300

    ij

---
+++ JS/js.js
@@ -0,0 +1,45 @@
+//1 - push e pop
+
+/*
+const array = ["a", "b", "c"];
+
+array.push("d");
+
+console.log(array);
+
+console.log(array.length);
+
+array.pop();
+
+console.log(array);
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido);
+
+console.log(array);
+
+array.push("z","x","y");
+
+console.log(array)
+
+*/
+
+//12 - shift e unshift
+
+/*
+const letters = ["a", "b", "c"];
+
+const letter = letters.shift();
+
+console.log(letter);
+
+console.log(letters);
+
+letters.unshift("p","q","r");
+
+letters.unshift("z");
+
+console.log(letters)*/
+
+//13 -
