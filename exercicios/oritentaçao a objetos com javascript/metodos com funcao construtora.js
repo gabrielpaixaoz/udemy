557cdd6905d01df24de208f8d2af3c74514fc7cc
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-08-12 17:35:22 -0300

    rfgdfeaf

--- exercicios/oritentaçao a objetos com javascript/metodos com funcao construtora.js
+++ exercicios/oritentaçao a objetos com javascript/metodos com funcao construtora.js
@@ -14,3 +14,18 @@ Cachorro.prototype.uivar = function(){
 console.log(Cachorro.prototype);
 
 husky.uivar()
+
+//classes es6
+
+class CachorroClasse{
+    constructor(nome,raca){
+        this.nome = nome;
+        this.raca = raca;
+    }
+}
+
+const jeff = new CachorroClasse("Jeff","Labrador");
+
+console.log(jeff);
+
+console.log(Object.getPrototypeOf(jeff))
