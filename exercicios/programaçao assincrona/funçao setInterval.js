9220567c915675aad14648901f0ca4e0ebae54f8
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-08-19 08:48:22 -0300

    efaseafeff

---
+++ exercicios/programaçao assincrona/funçao setInterval.js
@@ -0,0 +1,7 @@
+console.log("Ainda nao executou")
+
+setInterval(function(){
+    console.log("intervalo assíjncrono")
+},3000)
+
+console.log("Ainda nao executou 2")
---
+++ exercicios/programaçao assincrona/funçao setTimeout.js
@@ -0,0 +1,7 @@
+console.log("Ainda nao executou")
+
+setTimeout(() => {
+    console.log("Requisiçao assincrona")
+}, 2000);
+
+console.log("Ainda nao executou 2")
