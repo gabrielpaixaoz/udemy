382cc603197e0dbb7ca8832ed6718ef7f1ceaa46
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-08-29 20:09:20 -0300

    teste

--- exercicios/DOM/js/script.js
+++ exercicios/DOM/js/script.js
@@ -7,4 +7,14 @@ console.log(document.childNodes[1].childNodes)
 
 console.log(document.body.childNodes[1].childNodes[1].textContent)
 
-// 2 - selecionando por tag
+//2 - selecionadno os elementos por CSS
+
+
+
+const products = document.querySelectorAll(".product")
+
+console.log(products)
+
+const mainContainer = document.querySelector("#main-container")
+
+console.log(mainContainer)
