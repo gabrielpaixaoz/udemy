da02a613c26fd293bed9d7230625ef5262590d51
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-09-05 10:23:01 -0300

    daedaed

--- exercicios/Eventos/js/scripts.js
+++ exercicios/Eventos/js/scripts.js
@@ -22,3 +22,79 @@ otherBtn.addEventListener("click", () =>{
     console.log("Evento removido")
     secondBtn.removeEventListener("click", imprimirMensage)
 })
+
+
+//3 - argumento do evento
+
+const mytitle = document.querySelector("#my-title")
+
+mytitle.addEventListener("click", (event) =>{
+    console.log(event)
+    console.log(event.offsetX)
+    console.log(event.pointerType)
+    console.log(event.target)
+})
+
+
+//4 - PROPAGAÇAO
+
+
+const containerBTN = document.querySelector("#btn-container")
+
+const btnInsideContainer = document.querySelector("#div-btn")
+
+
+containerBTN.addEventListener("click",()=>{
+    console.log("Evento 1")
+})
+
+btnInsideContainer.addEventListener("click", (e)=>{
+    e.stopPropagation()
+    console.log("Evento 2")
+})
+
+//5 - removendo evento padrao
+
+
+const a = document.querySelector("a")
+
+a.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    console.log("Não alterou a página")
+})
+
+//6 - eventos de tecla
+
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`)
+})
+
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Apertou a tecla ${e.key}`)
+})
+
+//7- eventos de mouse
+
+const mouseEvents = document.querySelector('#mouse')
+
+mouseEvents.addEventListener("mousedown", () =>{
+    console.log("Pressionou")
+})
+
+mouseEvents.addEventListener("mouseup", () =>{
+    console.log("Soltou")
+})
+
+mouseEvents.addEventListener("dblclick", () =>{
+    console.log("clique duplo")
+})
+
+
+// 8 - movimento do mouse
+
+document.addEventListener("mousemove", (e) =>{
+    //console.log(`No eixo X: ${e.x}`)
+    //console.log(`No eixo Y: ${e.y}`)
+}) 
