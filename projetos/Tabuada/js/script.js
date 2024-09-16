23ae3ef55084473f4ca119a25461e43cd30acbe4
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-09-16 13:59:35 -0300

    bora

--- projetos/Tabuada/js/script.js
+++ projetos/Tabuada/js/script.js
@@ -1,36 +1,47 @@
+const MULTIPLICATION_FORM = document.querySelector("#multiplication-form")
+const NUMBER_INPUT = document.querySelector("#number")
+const MULTIPLICATION_INPUT = document.querySelector("#multiplier")
+const MULTIPLICATION_TABLE = document.querySelector("#multiplication-operations")
 
-const button = document.querySelector('#event')
-const output = document.querySelector('#output')
-let html = '<table border="1" cellpadding="5" cellspacing="0" width="500">'
 
+const CREATE_TABLE = (NUMBER, MULTIPLICATOR_NUMBER) =>{
+    MULTIPLICATION_TABLE.innerHTML = ""
 
-button.addEventListener("click",() => {
-const  numberHTML = Number(document.querySelector('#number').value)
-let multiplier = Number(document.querySelector('#multiplier').value)
+    for(i = 1; i <= MULTIPLICATOR_NUMBER; i++){
+        const RESULT = NUMBER * i
+        console.log(RESULT)
 
-if(numberHTML > 0 && multiplier > 0){
-
-    for(i = 1; i <= multiplier; i++){
-        html += `<tr><td>${numberHTML} x ${i} = ${numberHTML * i}</td></tr>`
-    }
-    
-    output.innerHTML = html
+        const TEMPLATE = `<div class="row">
+                <div class="opera"> ${NUMBER} x ${i} =</div>
+                <div class="result">${RESULT}</div>
+            </div>`
+          const PARSER = new DOMParser()
+          const HTML_TEMPLATE = PARSER.parseFromString(TEMPLATE, "text/html")
+          const ROW = HTML_TEMPLATE.querySelector(".row")
+          MULTIPLICATION_TABLE.appendChild(ROW)
+        }
 }
 
 
+
+MULTIPLICATION_FORM.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    const NUMBER = +NUMBER_INPUT.value
+
+    const MULTIPLICATOR_NUMBER = +MULTIPLICATION_INPUT.value
+
+    if(!NUMBER || !MULTIPLICATOR_NUMBER) return
     
+    CREATE_TABLE(NUMBER,MULTIPLICATOR_NUMBER)
 })
 
 
 
-
-
-/*
-  <table border="1" cellpadding="5" cellspacing="0" width="500">
-                    <tr>
-                        <td>teste</td>
-                    </tr>
-                    <tr>
-                        <td>teste</td>
-                    </tr>
-                  </table>*/
+/*<div class="row">
+                <div class="opera"> 5 x 1 =</div>
+                <div class="result">5</div>
+            </div>
+            <div class="row">
+                <div class="opera"> 5 x 2 =</div>
+                <div class="result">10</div>
+*/
